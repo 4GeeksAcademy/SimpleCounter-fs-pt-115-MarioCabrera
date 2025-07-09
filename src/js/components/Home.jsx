ead1cc48ec2467c4c02e7b40463e86040e4a88ca
Macalo1998 <110059967+Macalo1998@users.noreply.github.com>
2025-07-09 09:44:39 +0000

    terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,23 @@
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
 import { Contador } from "./Contador";
-import { variables } from "./variables";
-const contadores = variables[0].contadores
 //create your first component
-const Home = () => {
-
-	const [seconds, setSeconds] = useState(0);
-
-	useEffect(() => {
-		const intervalId = setInterval(() => {
-			setSeconds(prev => prev + 1);
-		}, 1000); // Actualiza cada 2 segundos
-
-		return () => clearInterval(intervalId);
-	}, []);
-	const secondsString = seconds.toString().padStart(6, "0"); // Ejemplo: 000005
-	const contador = secondsString.split("").map(Number);
+const Home = ({data}) => {
+	const {
+		counter,
+		cuentaRegresiva,
+		alternarDetencion,
+		estaDetenido,
+		limiteFinal,
+		esRegresiva
+	} = data;
+	const secondsString = counter.toString().padStart(6, "0");
+	const contador = secondsString.split("").map(char =>
+		char === "-" ? "-" : Number(char)
+	);
 	return (
 		<>
 			<div className="container-fluid  bg-dark p-5">
-				<div className="row d-flex justify-content-between">
+				<div className="row d-flex justify-content-between mb-4">
 					<div className="col-1 text-white fs-1 border border-secondary-subtle border-top-0 border-bottom-0 text-center rounded mx-2">
 						<i className="fa-solid fa-stopwatch"></i>
 					</div>
@@ -34,6 +30,11 @@ const Home = () => {
 							}}
 						/>
 					))}
+				</div>
+				<div className="row d-flex justify-content-between mb-4">
+					<button className="btn btn-primary col-3" onClick={cuentaRegresiva}>{esRegresiva ? "Cuenta progresiva": "Cuenta regresiva"}</button>
+					<button className={`btn ${estaDetenido ? "btn-success" : "btn-danger"} col-3`} onClick={alternarDetencion}> {estaDetenido ? "Start" : "Stop"}</button>
+					<button className="btn btn-info col-3" onClick={limiteFinal}>Determina el final</button>
 				</div>
 			</div>
 		</>
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -10,9 +10,41 @@ import '../styles/index.css'
 
 // components
 import Home from './components/Home';
+let counter = 0;
+let esRegresiva = false;
+let estaDetenido = false
+let limite;
+const cuentaRegresiva = () => {
+  counter = prompt("elige el nuevo número del contador", 0)
+  esRegresiva = !esRegresiva
+}
+const alternarDetencion = () => {
+  estaDetenido = !estaDetenido
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+}
+const limiteFinal = () => {
+  limite = prompt("Establece el número en el que debe acabar el número", 0)
+}
+
+const reactRender = ReactDOM.createRoot(document.getElementById('root'))
+setInterval(() => {
+  reactRender.render(
+    <React.StrictMode>
+      <Home data={{
+        counter: counter,
+        cuentaRegresiva: cuentaRegresiva,
+        alternarDetencion: alternarDetencion,
+        estaDetenido: estaDetenido,
+        limiteFinal: limiteFinal,
+        esRegresiva: esRegresiva
+      }} />
+    </React.StrictMode>,
+  )
+  if (!esRegresiva && !estaDetenido && limite == undefined) return counter++
+  if (esRegresiva && !estaDetenido && limite == undefined) return counter--
+  if (!esRegresiva && !estaDetenido && counter < limite) return counter++
+  if (esRegresiva && !estaDetenido && counter > limite) return counter--
+  if (counter == limite) return alert("Hemos llegado al número 😎"), counter = 0, limite = undefined
+  if (!esRegresiva && counter > limite) return alert("Ese límite ya lo pasamos 😅"), limite = undefined
+
+}, 1000);
